5abf966717c4c4b9e2357d32dfe47f9837c0481e
LasanaPahanga <lpahanga@gmail.com>
2026-01-17 23:22:19 +0530

    Fix undefined setter functions - add back required state variables

--- client/src/components/FacebookDownloader.js
+++ client/src/components/FacebookDownloader.js
@@ -36,6 +36,7 @@ const FacebookDownloader = () => {
   const [downloadStage, setDownloadStage] = useState('');
   
   // Server health states
+  const [serverHealth, setServerHealth] = useState(null);
   const [cookieWarning, setCookieWarning] = useState('');
   const [diskWarning, setDiskWarning] = useState('');
   
